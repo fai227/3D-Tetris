8ca9a428d3e57b45987d4a49b231fe70d2549998
fai227 <fai227@outlook.jp>
2023-04-04 16:18:54 +0900

    共有用にコミット

--- Assets/Script/Other/ThreeDText.cs
+++ Assets/Script/Other/ThreeDText.cs
@@ -5,13 +5,14 @@ using DG.Tweening;
 
 public class ThreeDText : MonoBehaviour
 {
+    [SerializeField] private float duration;
     private Material material;
     void Start()
     {
         material = GetComponent<MeshRenderer>().materials[0];
         material.color = new Color(1f, 1f, 1f, 0f);
         material.DOFade(1f, Option.FADE_DURATION);
-        DOVirtual.DelayedCall(Option.THREE_D_TEXT_DURATION + Option.FADE_DURATION, () =>
+        DOVirtual.DelayedCall(duration + Option.FADE_DURATION, () =>
         {
             material.DOFade(0f, Option.FADE_DURATION);
             Destroy(gameObject, Option.FADE_DURATION);
